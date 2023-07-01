4941a5a4e681cb33b74a5d8472190acd7e378ae0
Avatar_amir <baimuratovamir@yandex.com>
2023-07-01 16:52:55 +0500

    Creating project

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.myapplication.databinding.ActivityMainBinding
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var bindingClass : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+    }
+
+  
+}
